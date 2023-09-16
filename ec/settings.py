893bc088ee829b78617460e0b2f6e4f64d1a729a
varundwivedi77 <109964409+varundwivedi77@users.noreply.github.com>
2023-09-16 14:04:47 +0530

    Update settings.py

--- ec/settings.py
+++ ec/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)$@e$#8zu-m3rnqlg79sz(#f9!znwo5k((tbf!+wn^=&a*-*t1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
